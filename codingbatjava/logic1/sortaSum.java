89822d1024e2bc48334be34cb00e84e884603a8d
llv8155 <81585305+llv8155@users.noreply.github.com>
2021-03-30 09:43:39 -0700

    Create sortaSum.java

---
+++ codingbatjava/logic1/sortaSum.java
@@ -0,0 +1,22 @@
+/*
+
+Given 2 ints, a and b, return their sum. However, sums in the range 10..19 inclusive, are forbidden, so in that case just return 20.
+
+
+sortaSum(3, 4) → 7
+sortaSum(9, 4) → 20
+sortaSum(10, 11) → 21
+*/
+
+public int sortaSum(int a, int b) {
+  if (a + b >= 10 && a + b <= 19)
+ // However, sums in the range 10..19 inclusive, are forbidden, so in that case just return 20
+  {
+    return 20;
+  }
+  else
+  {
+    return a + b;
+    // return their sum
+  }
+}
