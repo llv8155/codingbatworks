aa6f55fcd80bb5df5778b03e508c37a0d8f2e5da
llv8155 <81585305+llv8155@users.noreply.github.com>
2021-04-10 11:11:57 -0700

    Update isEverywhere.java

--- codingbatjava/array2/isEverywhere.java
+++ codingbatjava/array2/isEverywhere.java
@@ -9,24 +9,25 @@ isEverywhere([1, 2, 1, 3, 4], 1) → false
 */
 
 public boolean isEverywhere(int[] nums, int val) {
+  // flag1 checks first every other pair
   boolean flag1 = true;
+  // flag2 checks second every other pair
   boolean flag2 = true;
  
-    for (int i = 0; i < nums.length; i += 2)
-    {
-      if (nums[i] != val)
-      {
+    for (int i = 0; i < nums.length; i += 2) {
+      // index 0 and index 2 then so on
+      if (nums[i] != val) {
         flag1 = false;
       }
     }
  
-    for (int i = 0; i < nums.length - 1; i += 2)
-    {
-       if (nums[i + 1] != val) 
-       {
+    for (int i = 0; i < nums.length - 1; i += 2) {
+       // index 1 and index 3 then so on
+       if (nums[i + 1] != val) {
          flag2 = false;
        }
     }
-
+    
+    // returns true if only one of the flags are true
     return flag1 || flag2;
 }
