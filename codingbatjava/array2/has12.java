0bcd45eafc577de088351c92a55d2028068af2be
llv8155 <81585305+llv8155@users.noreply.github.com>
2021-04-16 22:23:59 -0700

    Update has12.java

--- codingbatjava/array2/has12.java
+++ codingbatjava/array2/has12.java
@@ -8,19 +8,14 @@ has12([3, 1, 2]) → true
 has12([3, 1, 4, 5, 2]) → true
 */
 
-public boolean has12(int[] nums)
-{
+public boolean has12(int[] nums) {
   int one = 0, two = 0;
-  for (int i = 0; i < nums.length; i++)
-  {
-    if (nums[i] == 1)
-    {
-      while (nums[i] != 2 && i != nums.length-1)
-      {
+  for (int i = 0; i < nums.length; i++) {
+    if (nums[i] == 1) {
+      while (nums[i] != 2 && i != nums.length-1) {
         i++;
       }
-      if (nums[i] == 2)
-      {
+      if (nums[i] == 2) {
         return true;
       }
     }
